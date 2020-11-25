6b67f9674ef12c02a1f69d83f5ccc64e3ac6d65e
1808561080_Calvin Christian <68107761+calvin15k1@users.noreply.github.com>
2020-11-25 20:52:39 +0700

    Add files via upload

---
+++ Tugas 6-Form Validation/script.js
@@ -0,0 +1,8 @@
+function validasiEmail() {
+	var rs = document.forms["formInput"]["email"].value;
+	var atps=rs.indexOf("@");
+	var dots=rs.lastIndexOf(".");
+	if (atps<1 || dots<atps+2 || dots+2>=rs.length) {
+		alert("Alamat email tidak valid.");
+	}
+}
